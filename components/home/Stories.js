42a7f0a9cdfbc79a1e1ebdc1dafa6fff7806896c
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 15:24:00 +0200

    Rokas

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ScrollView, Image} from 'react-native';
 import {USERS} from '../../data/stories';
 
 const Stories = () => {
     return (
         <View>
-
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal >
+                {USERS.map((story, index) => {
+                    return (
+                        <View key={index} style={styles.user}>
+                            <Image style={styles.story} source={{ uri: story.image }} />
+                        </View>
+                    )
+                })}
+            </ScrollView>
         </View>
     );
 };
 
 export default Stories;
+
+const styles = StyleSheet.create({
+    user: {
+        marginRight: 10,
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#ddd',
+    },
+    story: {
+        width: 70,
+        height: 70,
+    },
+});
