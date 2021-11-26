8bdc5156c84618bed506bf0cfbdb9a2476fbecda
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 15:13:08 +0200

    Rokas

---
+++ components/home/Stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Stories = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default Stories;
