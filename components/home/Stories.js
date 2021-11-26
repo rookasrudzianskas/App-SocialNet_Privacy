e7c45b876977d5e3ecdf8f19c6280bf3e85585fb
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 20:40:50 +0200

    Rokas

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -8,7 +8,8 @@ const Stories = () => {
             <ScrollView showsHorizontalScrollIndicator={false} horizontal >
                 {USERS.map((story, index) => {
                     return (
-                        <View key={index} style={{justifyContent: 'center', alignItems: 'center', marginHorizontal: 2, }}>
+                        <View key={index} style={{justifyContent: 'center', alignItems: 'center', marginHorizontal: 2, marginLeft: 6,
+                        }}>
                             <Image style={styles.story} source={{ uri: story.image }} />
                             <Text style={{color: 'white'}}>{story.user.length > 11 ? story.user.slice(0, 10).toLowerCase() + '...' : story.user.toLowerCase()}</Text>
                         </View>
@@ -25,7 +26,6 @@ const styles = StyleSheet.create({
     story: {
         width: 70,
         height: 70,
-        marginLeft: 6,
         borderWidth: 3,
         borderRadius: 50,
         borderColor: '#ff8501',
