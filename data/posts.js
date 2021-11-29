7dd1c9fca2f15e447793295902de6d61c8227574
Rokas <rookas.rudzenskas@gmail.com>
2021-11-29 11:12:46 +0200

    Rokas

--- data/posts.js
+++ data/posts.js
@@ -1,27 +1,27 @@
 import {USERS} from "./stories";
 
 export const POSTS = [
-    {
-        imageUrl: 'https://cdn.truelancer.com/user-picture/562992-5b86c26e23727.jpg',
-        user: USERS[0].user,
-        likes: 5454,
-        caption: 'Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓',
-        profile_picture: USERS[0].image,
-        comments: [
-            {
-                user: 'rokas',
-                comment: 'Wow! This is an epic picture, where have you been bro? Is it real?',
-            },
-            {
-                user: 'rokas.dev',
-                comment: 'I\'m not sure if this is real, but I\'m pretty sure it is.',
-            },
-            {
-                user: 'rokas.dev',
-                comment: 'I\'m not sure if this is real, but I\'m pretty sure it is.',
-            },
-        ],
-    },
+    // {
+    //     imageUrl: 'https://cdn.truelancer.com/user-picture/562992-5b86c26e23727.jpg',
+    //     user: USERS[0].user,
+    //     likes: 5454,
+    //     caption: 'Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓, Train to ride to Hogwarts 🌓',
+    //     profile_picture: USERS[0].image,
+    //     comments: [
+    //         {
+    //             user: 'rokas',
+    //             comment: 'Wow! This is an epic picture, where have you been bro? Is it real?',
+    //         },
+    //         {
+    //             user: 'rokas.dev',
+    //             comment: 'I\'m not sure if this is real, but I\'m pretty sure it is.',
+    //         },
+    //         {
+    //             user: 'rokas.dev',
+    //             comment: 'I\'m not sure if this is real, but I\'m pretty sure it is.',
+    //         },
+    //     ],
+    // },
     {
         imageUrl: 'https://about.me/cdn-cgi/image/q=80,dpr=1,f=auto,fit=cover,w=1200,h=630,gravity=0.474x0.27/https://assets.about.me/background/users/r/o/o/rookas_1585854606_159.jpg',
         user: USERS[1].user,
