85f9f0c2b696685ef70ee483c3a393fc2df9f906
Rokas <rookas.rudzenskas@gmail.com>
2021-11-29 11:33:22 +0200

    Rokas

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import 'core-js/es6/promise';
+import 'core-js/es6/set';
+import 'core-js/es6/map';
+import * as yup from 'yup';
+
 
 const FormikPostUploader = () => {
+
+    const uploadPostSchema = yup.object().shape({
+        imageUrl: yup.string().url().required('A URL is required'),
+        caption: yup.string().max(2200, 'A caption has reached the character limit.'),
+
+    })
+
     return (
         <View>
             <Text style={{color: 'white'}}>FORMIX</Text>
